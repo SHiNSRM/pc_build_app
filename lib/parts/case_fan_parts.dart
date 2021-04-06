43e16c4344e59a53ab81a3bd427e98edb3599156
Syunki0403 <Syunki0403@users.noreply.github.com>
2021-04-06 19:58:38 +0900

    フッターとパーツページの雛型

---
+++ lib/parts/case_fan_parts.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PartsCaseFan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ケースファン'),
+      ),
+      body: const Center(child: Text('ケースファン')),
+    );
+  }
+}
